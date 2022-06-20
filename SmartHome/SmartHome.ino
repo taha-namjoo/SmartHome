69d1d2aa40dfd0173131eeceaa6fde91acd5e707
taha-namjoo <tahanamjoo16@gmail.com>
2022-06-20 12:44:23 +0430

    Doc|ArduinoCode

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -1,28 +1,35 @@
 #include <SoftwareSerial.h>
-SoftwareSerial HM10(19, 18); // RX = 19, TX = 18
+
 int ledpin=13;
 int ACrun=2;
 int ACslow=3;
 int ACfast=4;
-
+int fpot=5;
+int lamp=6;
+int lock=7;
+int Humid_Temp=23;
 char appData;
-int unlock=5;
+//int unlock=5;
 int ACstatus=0;
 String inData = "";
 void setup(){
   pinMode(ACrun, OUTPUT);
   pinMode(ACslow, OUTPUT);
   pinMode(ACfast, OUTPUT);
-  pinMode(unlock, OUTPUT);
+  pinMode(fpot, OUTPUT);
+  pinMode(lamp, OUTPUT);
+  pinMode(lock, OUTPUT);
   digitalWrite(ACrun,HIGH);
   digitalWrite(ACslow,HIGH);
   digitalWrite(ACfast,HIGH);
-  digitalWrite(unlock,HIGH);
+  digitalWrite(fpot,HIGH);
+  digitalWrite(lamp,HIGH);
+  digitalWrite(lock,HIGH);
   
   Serial1.begin(9600);
-  Serial.begin(3600);
+  Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
-  HM10.begin(9600); 
+ // HM10.begin(9600); 
   delay(100);
   
   pinMode(ledpin,OUTPUT);
@@ -31,79 +38,150 @@ void setup(){
   ACstatus=0;
 }
 void loop(){
-//  digitalWrite(ACrun,HIGH);
-//  delay(2000);
-//  digitalWrite(ACrun,LOW);
-//  delay(2000);
-//  digitalWrite(ACrun,);
-  //delay(2000);
+  delay(500);
+  int soil_moisture=analogRead(A0); 
+  Serial.print("analog value: ");
+  Serial.println(soil_moisture);
 
- // Serial.println("DATA RECEIVED:"); 
-  HM10.listen();  // listen the HM10 port
-  while (HM10.available() > 0) {   // if HM10 sends something then read
-    appData = HM10.read();
-    inData = String(appData);  // save the data in string format
-    Serial.write(appData);
-  }
-    if (Serial.available()) {           // Read user input if available.
+  if(soil_moisture<30) {
 
-    delay(10);
-
-    HM10.write(Serial.read());
+    Serial.println("Dry soil");
 
   }
-    //Serial.print((char)Serial1.read());  
-    //Serial1.println("DATA RECEIVED:");
 
-    if(inData=="SLOW"){
-     
-      if(ACstatus==0){
-        
-        digitalWrite(ACrun,1);
-        digitalWrite(ACslow,1);
-        ACstatus=2;
-      }
-      if(ACstatus==1){
-       digitalWrite(ACslow,1);
-       ACstatus=2;
-      }
-      if(ACstatus==3){
-       digitalWrite(ACfast,0);
-       ACstatus=2;
-      }
-    }
-     if(inData=='FAST'){
+  if((soil_moisture>300)&&(soil_moisture<700)) {
+
+    Serial.println("Humid soil");
+
+  }
+
+  if((soil_moisture>700)&&(soil_moisture<950)){
+    Serial.println("water");
+    digitalWrite(6,LOW);
+  }
+
+/////////////////////////////////////////////////////////////
+
+  if (Serial1.available()) {           // Read user input if available.
+    
+    delay(100);
+
+    
+    delay(100);
+    appData=Serial1.read();
+
+  }
+  if(appData=='N'){   
+    digitalWrite(ACrun,1);
+  
+    digitalWrite(ACslow,1);
+    digitalWrite(ACfast,1);
+    ACstatus=0;
+    Serial.print("temp=");
+    Serial.print(Humid_Temp);
+  }
+  if(appData=='0'){
       
       if(ACstatus==0){
-        digitalWrite(ACrun,1);
+        digitalWrite(ACrun,0);
         digitalWrite(ACslow,1);
         digitalWrite(ACfast,1);
         ACstatus=1;
       }
       if(ACstatus==1){
+        digitalWrite(ACrun,0);
         digitalWrite(ACslow,1);
         digitalWrite(ACfast,1);
+        ACstatus=1;
       }
       if(ACstatus==2){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,1);
         digitalWrite(ACfast,1);
+        ACstatus=1;
       }
+      if(ACstatus==3){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,1);
+        digitalWrite(ACfast,1);
+        ACstatus=1;
+      }
+        Serial.print("temp=");
+        Serial.print(Humid_Temp);
   
     }
-//    if(i=="ACoff"){
-//      digitalWrite(ACrun,0);
-//      digitalWrite(ACslow,0);
-//      digitalWrite(ACfast,0);
-//      ACstatus=0;
-//    }
-    if(inData=='N')
-    {
+    if(appData=='1'){
+     
+      if(ACstatus==0){
+        digitalWrite(ACrun,0);
+        delay(1000);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,1);
+        ACstatus=2;
+      }
+      if(ACstatus==1){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,1);
+        ACstatus=2;
+      }
+      if(ACstatus==2){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,1);
+        ACstatus=2;
+      }
+      if(ACstatus==3){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,1);
+        ACstatus=2;
+      }
+        Serial.print("temp=");
+        Serial.print(Humid_Temp);      
+    }
+    if(appData=='2'){
       
-      digitalWrite(ledpin,1);
-      Serial.println("led on");
+      if(ACstatus==0){
+        digitalWrite(ACrun,0);
+        delay(1000);
+        digitalWrite(ACslow,0);
+        delay(500);
+        digitalWrite(ACfast,0);
+        ACstatus=3;
+      }
+      if(ACstatus==1){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        delay(500);
+        digitalWrite(ACfast,0);
+        ACstatus=3;
+      }
+      if(ACstatus==2){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,0);
+        ACstatus=3;
+      }
+      if(ACstatus==3){
+        digitalWrite(ACrun,0);
+        digitalWrite(ACslow,0);
+        digitalWrite(ACfast,0);
+        ACstatus=3;
+      }
+      Serial.print("temp=");
+      Serial.print(Humid_Temp); 
     }
-    if(inData=='F')
-    {
-      digitalWrite(ledpin,0);
-      Serial.println("led off");
+    if(appData=='O'){
+      digitalWrite(lock,0);
+      delay(1500);
+      digitalWrite(lock,1);
+      appData="";
     }
-  }
+     if(appData=='L'){
+      digitalWrite(lamp,0);
+      delay(1500);
+      digitalWrite(lamp,1);
+      appData="";
+    }
+}
